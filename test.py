98ad3645f1432ad631589f51ff1048d75b96543a
Ryota_Ohno <ohno.ryota0805@gmail.com>
2023-04-21 23:40:02 +0900

    commit

--- test.py
+++ test.py
@@ -1,4 +1,13 @@
 import torch
-x = torch.rand(5, 3)
-print(x)
+print(torch.cuda.get_device_name())
 print(torch.__version__, torch.cuda.is_available())
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+print(device)
+for i in range(500):
+    a = torch.randn(5)
+    b = torch.randn(5)
+
+    a = a.to(device)
+    b = b.to(device)
+
+    print(a + b)
